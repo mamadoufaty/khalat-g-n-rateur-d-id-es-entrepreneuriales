938d5f3cdbfbccb3c1d20867a20fb97c9ac65531
Mamadou Faty <mamadoufaty@gmail.com>
2025-11-18 13:41:49 +0100

    First commit: Business Idea Generator

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <title>Mina a une Idée</title>
+        <meta name="description" content="Une IA pour concevoir des idées de business." />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
